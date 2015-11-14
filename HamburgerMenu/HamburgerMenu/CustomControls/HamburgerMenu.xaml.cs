afaba5804feedcf210669e2aa17e561bc119032d
Francesco Bonacci <francibon93@hotmail.it>
2015-11-14 15:53:19 +0100

    Added First Version of HamburgerMenu

---
+++ HamburgerMenu/HamburgerMenu/CustomControls/HamburgerMenu.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace HamburgerMenu.CustomControls
+{
+    public sealed partial class HamburgerMenu : UserControl
+    {
+        public HamburgerMenu()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BackRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HamburgerRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            // don't let the radiobutton check
+            (sender as RadioButton).IsChecked = false;
+
+            // toggle the splitview pane
+            this.ShellSplitView.IsPaneOpen = !this.ShellSplitView.IsPaneOpen;
+        }
+    }
+}
